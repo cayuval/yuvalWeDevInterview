0795cf039e5de8202b7f6f3cf0ed99a2565983a6
cayuval <jubim1234@gmail.com>
2023-05-02 19:44:23 +0300

    basic upload and get images

--- yuvalInter/src/App.jsx
+++ yuvalInter/src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AWS from 'aws-sdk';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageUrls, setImageUrls] = useState([]);
+  AWS.config.update({
+    accessKeyId: 'AKIAVFYA5WWID6TZJWVH',
+    secretAccessKey: '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',
+    region: 'us-east-2'
+  });
+  const s3 = new AWS.S3();
+  useEffect(() => {
+    const params = {
+      Bucket: 'yuvalsbucket09101999',
+    };
+
+    s3.listObjects(params, (err, data) => {
+      if (err) {
+        console.log('Error:', err);
+      } else {
+        const urls = data.Contents.map((obj) => {
+          return s3.getSignedUrl('getObject', { Bucket: params.Bucket, Key: obj.Key });
+        });
+        setImageUrls(urls);
+      }
+    });
+  }, []);
+
+  const handleFileInput = (event) => {
+
+
+    const file = event.target.files[0];
+
+    const params = {
+      Bucket: 'yuvalsbucket09101999',
+      Key: file.name,
+      Body: file,
+      ContentType: file.type,
+      ACL: 'public-read'
+    };
+
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Successfully uploaded image.');
+      }
+    });
+  }
+
+
 
   return (
     <>
+      <input type="file" onChange={handleFileInput} />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {imageUrls.map((url, index) => (
+          <img key={index} src={url} alt={`Image ${index}`} height="100px" />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
